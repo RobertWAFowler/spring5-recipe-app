4aa5b23c1466c6268b0d341cae9ce0a606e380a5
Robert Fowler <robertf@ipay.co.za>
2018-09-18 14:03:12 +0200

    216. Spring MVC Exception Handler

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -106,14 +106,12 @@ public class RecipeControllerTest {
     }
 
     @Test
-    public void testGetRecipeNotFount() throws Exception {
-
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
+    public void testGetRecipeNotFound() throws Exception {
 
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 }
